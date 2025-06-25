9b5d2f8fb32f1c0f0f40b92c49bfa87682a77052
João Feula <jfcfeula@gmail.com>
2025-06-25 17:53:18 -0300

    Update conversao.js

--- conversao.js
+++ conversao.js
@@ -17,7 +17,7 @@ i.question('Digite a chave do par: ', (chavePar) => {
     const obj = parParaObjeto([chavePar, valorPar]);
     console.log('Objeto criado a partir do par:', obj);
 
-    i.question('\nDigite a chave da propriedade do objeto: ', (chaveObj) => {
+    i.question('Digite a chave da propriedade do objeto: ', (chaveObj) => {
       i.question('Digite o valor da propriedade do objeto: ', (valorObj) => {
         const par = objetoParaPar({ [chaveObj]: valorObj });
         console.log('Par criado a partir do objeto:', par);
